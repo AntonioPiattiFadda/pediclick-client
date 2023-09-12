7da32aad151b761192e6ce6319895ed19b5a1760
Antonio Piatti Fadda <antonio.piattifadda@gmail.com>
2023-09-12 16:27:18 -0300

    footer's styles

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import Carrito from '../../assets/svg/carrito.svg';
 
 function Footer() {
   return (
     <footer className={styles['footer-container']}>
-      <div className={styles['footer-info']}>
-        <img src="" alt="Carrito" />
-        <span>Ir a tu carrito</span>
+      <div className={styles['footer-card']}>
+        <div className={styles['footer-info']}>
+          <img className={styles['footer-icon']} src={Carrito} alt="Carrito" />
+          <span>Ir a tu carrito</span>
+        </div>
       </div>
     </footer>
   );
