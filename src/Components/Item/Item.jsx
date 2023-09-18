2e7732395da4c21a19f28ee410d6e21698fb3b6b
Antonio Piatti <antonio.piattifadda@gmail.com>
2023-09-18 17:06:44 -0300

    estilos texto y no stock

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,18 +7,28 @@ const Item = ({ element }) => {
     <>
       <Link
         className={styles.cardContainer}
-        disabled={element.blocked}
-        to={`/itemDetail/${element.id}`}
+        to={element.blocked ? undefined : `/itemDetail/${element.id}`}
       >
         <div className={styles.cardInfo}>
           <span className={styles.cardName}>{element.name}</span>
-          <span className={styles.cardPrice}>${element.price}</span>
+          <span className={styles.cardPrice}>
+            {' '}
+            <span className={styles.cardFrom}>desde</span>${element.price}
+          </span>
         </div>
-        {/* {element.blocked && (
-              <div className={styles.noStockOvelay}>
-                <span>No hay stock</span>
-              </div>
-            )} */}
+        {element.blocked && (
+          <div
+            style={{
+              position: 'absolute',
+              width: '100%',
+              height: ' 100%',
+              zIndex: '1200',
+            }}
+          >
+            <div className={styles.noStockOvelay}></div>
+            <span className={styles.noStockSpan}>sin stock disponible</span>
+          </div>
+        )}
         <img className={styles.cardImage} src={element.image} alt="" />
       </Link>
     </>
